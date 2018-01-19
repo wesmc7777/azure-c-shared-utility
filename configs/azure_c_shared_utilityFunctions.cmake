b8605d64d344b034fc9439e6c98fc1cd1179f516
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2018-01-19 13:32:53 -0800

    Prepare test functions to support longhaul tests

--- configs/azure_c_shared_utilityFunctions.cmake
+++ configs/azure_c_shared_utilityFunctions.cmake
@@ -197,7 +197,8 @@ function(build_test_artifacts whatIsBuilding use_gballoc)
         if(
             (("${whatIsBuilding}" MATCHES ".*ut.*") AND ${run_unittests}) OR
             (("${whatIsBuilding}" MATCHES ".*e2e.*") AND ${run_e2e_tests}) OR
-            (("${whatIsBuilding}" MATCHES ".*int.*") AND ${run_e2e_tests})
+            (("${whatIsBuilding}" MATCHES ".*int.*") AND ${run_e2e_tests}) OR
+            (("${whatIsBuilding}" MATCHES ".*longhaul.*") AND ${run_longhaul_tests})
         )
                 windows_unittests_add_exe(${whatIsBuilding} ${ARGN})
                 if (${use_cppunittest})
@@ -217,6 +218,7 @@ function(build_test_artifacts whatIsBuilding use_gballoc)
             (("${whatIsBuilding}" MATCHES ".*ut.*") AND ${run_unittests}) OR
             (("${whatIsBuilding}" MATCHES ".*e2e.*") AND ${run_e2e_tests}) OR
             (("${whatIsBuilding}" MATCHES ".*int.*") AND ${run_e2e_tests})
+            (("${whatIsBuilding}" MATCHES ".*longhaul.*") AND ${run_longhaul_tests})
         )
             linux_unittests_add_exe(${whatIsBuilding} ${ARGN})
         endif()
@@ -453,7 +455,8 @@ function(build_c_test_artifacts whatIsBuilding use_gballoc folder)
         if(
             (("${whatIsBuilding}" MATCHES ".*ut.*") AND ${run_unittests}) OR
             (("${whatIsBuilding}" MATCHES ".*e2e.*") AND ${run_e2e_tests}) OR
-            (("${whatIsBuilding}" MATCHES ".*int.*") AND ${run_e2e_tests})
+            (("${whatIsBuilding}" MATCHES ".*int.*") AND ${run_e2e_tests}) OR
+            (("${whatIsBuilding}" MATCHES ".*longhaul.*") AND ${run_longhaul_tests})
         )
             if (${use_cppunittest})
                 c_windows_unittests_add_dll(${whatIsBuilding} ${folder} ${ARGN})
@@ -472,7 +475,8 @@ function(build_c_test_artifacts whatIsBuilding use_gballoc folder)
         if(
             (("${whatIsBuilding}" MATCHES ".*ut.*") AND ${run_unittests}) OR
             (("${whatIsBuilding}" MATCHES ".*e2e.*") AND ${run_e2e_tests}) OR
-            (("${whatIsBuilding}" MATCHES ".*int.*") AND ${run_e2e_tests})
+            (("${whatIsBuilding}" MATCHES ".*int.*") AND ${run_e2e_tests}) OR
+            (("${whatIsBuilding}" MATCHES ".*longhaul.*") AND ${run_longhaul_tests})
         )
             c_linux_unittests_add_exe(${whatIsBuilding} ${folder} ${ARGN})
         endif()
