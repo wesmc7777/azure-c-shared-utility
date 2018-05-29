6b8bff0405f877c6bdbb9ac305572256f3a92482
Roy Sprowl <roys@jetstreamsoftware.com>
2018-05-29 14:52:47 -0700

    ...

--- adapters/tlsio_schannel.c
+++ adapters/tlsio_schannel.c
@@ -1129,16 +1129,9 @@ void tlsio_schannel_destroy(CONCRETE_IO_HANDLE tls_io)
     }
 }
 
-static bool fail_now = true;
 int tlsio_schannel_open(CONCRETE_IO_HANDLE tls_io, ON_IO_OPEN_COMPLETE on_io_open_complete, void* on_io_open_complete_context, ON_BYTES_RECEIVED on_bytes_received, void* on_bytes_received_context, ON_IO_ERROR on_io_error, void* on_io_error_context)
 {
     int result;
-
-    if (fail_now)
-    {
-        fail_now = false;
-        return -1;
-    }
 
     if (tls_io == NULL)
     {
